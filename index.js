ce17a6492733cb5c5e453190c53d4007950efc59
mg-blvd2 <mguijarro@csumb.edu>
2020-03-11 15:09:17 -0700

    pushing

---
+++ index.js
@@ -0,0 +1,39 @@
+/* EJS - Embedded JavaScript
+    1. NodeJS looks for ejs files in "views"
+    2. ejs files end with .ejs
+    3. Have to let the NodeJS engine know that we are using EJS
+*/
+const port = 3000;
+var express = require("express");
+var bodyParser = require("body-parser")
+var app = express();
+
+app.use(express.static("css"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  //res.send("HELLO WORLD");
+  res.render("home");
+});
+
+var friendList = ["Alice", "Clark", "Brllrmy", "Octavia"];
+app.get("/friends", function(req, res){
+  res.render("friends", {friends: friendList});
+});
+
+app.post("/addfriend", function(req, res){
+  var newfriend = req.body.newFriend;
+  friendList.push(newfriend);
+  res.redirect("/friends")
+  console.log(req.body);
+});
+
+app.get("*", function(req, res){
+  //res.send("Page not found");
+  res.render("error");
+});
+
+app.listen(port, function(){
+  console.log("Server is up and running!");
+});
